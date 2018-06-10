1c00935379e444e8f56e4cefc4492301687957f1
NickConnor <nickconnor52@gmail.com>
2018-06-10 11:00:11 -0400

    Added team logos

--- src/components/Matchups.vue
+++ src/components/Matchups.vue
@@ -6,8 +6,19 @@
     <div class="row">
       <div class="col-md-12 card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">This is where we will display the head to head matchups</p>
+          <div class="row">
+            <div class="offset-md-2 col-md-4">
+              <div class="card-body logo">
+                <img src="../assets/0020919_cincinnati-bengals_300.png" alt="bengals"/>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card-body logo">
+                <img src="../assets/0020920_dallas-cowboys_300.png" alt="bengals"/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -18,3 +29,9 @@ export default {
   name: 'matchups'
 }
 </script>
+<style>
+.logo {
+  vertical-align: middle;
+  border: solid black;
+}
+</style>
