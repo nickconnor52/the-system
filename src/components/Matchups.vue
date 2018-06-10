9f42bea03071744ce78bdb787ef561c15a22deff
NickConnor <nickconnor52@gmail.com>
2018-06-10 02:31:43 -0400

    Added a card

--- src/components/Matchups.vue
+++ src/components/Matchups.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id="matchups">
-      <h1>
-          sup
-      </h1>
+  <div class="container" id="matchups">
+    <h1>
+        Matchups
+    </h1>
+    <div class="row">
+      <div class="col-md-12 card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
