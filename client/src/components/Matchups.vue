a60a11dc40baaeeae25c987f6abb7c9f49ebb989
NickConnor <nickconnor52@gmail.com>
2018-07-12 22:29:32 -0400

    Adjusting styles

--- client/src/components/Matchups.vue
+++ client/src/components/Matchups.vue
@@ -11,11 +11,13 @@
             <div class="offset-md-2 col-md-4">
               <div class="card-body logo">
                 <img src="../assets/0020919_cincinnati-bengals_300.png" alt="bengals"/>
+                <h4 class="text-dark">Bengals</h4>
               </div>
             </div>
             <div class="col-md-4">
               <div class="card-body logo">
                 <img src="../assets/0020920_dallas-cowboys_300.png" alt="bengals"/>
+                <h4 class="text-dark">Cowboys</h4>
               </div>
             </div>
           </div>
@@ -33,5 +35,8 @@ export default {
 .logo {
   vertical-align: middle;
   border: solid black;
+}
+img {
+  height: 80px;
 }
 </style>
