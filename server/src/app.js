151400b63f8cc11fc80302049e5ff985848587a6
NickConnor <nickconnor52@gmail.com>
2018-07-14 17:37:46 -0400

    Removed deprecated

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,6 @@ app.use(cors())
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/systemdb');
 var db = mongoose.connection;
-var collection = db.collection('teams')
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
   console.log("Connection Succeeded");
