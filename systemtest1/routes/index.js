d0595cafe7444fd59faee48f137fbae1d878caad
NickConnor <nickconnor52@gmail.com>
2018-07-14 15:53:18 -0400

    Toying with express

--- systemtest1/routes/index.js
+++ systemtest1/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET teams page. */
+router.get('/teams', function(req, res, next) {
+  res.render('teams', { title: 'TeamList' });
+});
+
 module.exports = router;
