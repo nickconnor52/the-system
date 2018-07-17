005120b4171c0b0c3985a8a34638373f5859232a
NickConnor <nickconnor52@gmail.com>
2018-07-16 23:36:51 -0400

    Added teams to routes

--- systemexpress/routes/index.js
+++ systemexpress/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+var Team = require("../models/team");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
