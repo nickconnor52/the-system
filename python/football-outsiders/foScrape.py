4381376db4399478a17d463aef382f49f4645d6d
Nick Connor <nickconnor52@gmail.com>
2018-07-18 14:46:48 -0400

    Adjusted path and added dependencies

--- python/football-outsiders/foScrape.py
+++ python/football-outsiders/foScrape.py
@@ -5,12 +5,12 @@ week = '0'
 
 # LOS Data
 tables = pd.read_html("http://www.footballoutsiders.com/stats/drivestats", header=0)
-tables[1].to_json('../storage/football-outsiders/week-' + week + '/LOS.json')
+tables[1].to_json('../storage/football-outsiders/week_' + week + '_LOS.json')
 
 # OFF pts/RZ
 tables = pd.read_html("http://www.footballoutsiders.com/stats/drivestatsoff", header=0)
-tables[1].to_json('../storage/football-outsiders/week-' + week + '/OFF_pts_rz.json')
+tables[1].to_json('../storage/football-outsiders/week_' + week + '_OFF_pts_rz.json')
 
 # DEF pts/RZ
 tables = pd.read_html("http://www.footballoutsiders.com/stats/drivestatsdef2017", header=0)
-tables[1].to_json('../storage/football-outsiders/week-' + week + '/DEF_pts_rz.json')
+tables[1].to_json('../storage/football-outsiders/week_' + week + '_DEF_pts_rz.json')
