84f920da3c9a6f4e1ce26e0c75be204c262fa33b
Nick Connor <nickconnor52@gmail.com>
2018-07-18 23:22:22 -0400

    Added lots - can add JSON with Off/Def los/dr

--- python/football-outsiders/foScrape.py
+++ python/football-outsiders/foScrape.py
@@ -7,28 +7,73 @@ week = '0'
 
 # LOS Data
 tablesDrive = pd.read_html("http://www.footballoutsiders.com/stats/drivestats", header=0)
-driveJson = tablesDrive[1].to_json()
+driveJson = json.loads(tablesDrive[1].to_json())
 
 
 # OFF pts/RZ
 tablesOff = pd.read_html("http://www.footballoutsiders.com/stats/drivestatsoff", header=0)
-offJson = tablesOff[1].to_json()
+offJson = json.loads(tablesOff[1].to_json())
 
 # DEF pts/RZ
 tablesDef = pd.read_html("http://www.footballoutsiders.com/stats/drivestatsdef2017", header=0)
-defJson = tablesDef[1].to_json()
+defJson = json.loads(tablesDef[1].to_json())
 
-# Cleaned Up JSON
-
-finalJson = {}
-
-# Ingest into DB
+# Connect to DB
 
 client = MongoClient('mongodb://nickconnor52:Cardinals77@ds137611.mlab.com:37611/systemdb')
 db = client['systemdb']
-collection_fo = db['football_outsiders']
+stats_collection = db['stats']
+weeks_collection = db['weeks']
+teams_collection = db['teams']
 
-collection_fo.insert(file_data)
+# Cleaned Up JSON
+
+defTeams = defJson['Team']
+driveTeams = driveJson['Team']
+offTeams = offJson['Team']
+
+finalJson = {}
+
+# DRIVE pts/rz
+for num, team in driveTeams.items():
+    if(teams_collection.find_one({'nickname': team})):
+        thisTeam = teams_collection.find_one({'nickname': team})
+        finalJson[thisTeam['name']] = {
+            'team': thisTeam['_id'],
+            'off_los_drive': driveJson['OFF.LOS/Dr'][num].split(' ')[0],
+            'def_los_drive': driveJson['DEF.LOS/Dr'][num].split(' ')[0],
+        }
+
+# OFF pts/rz
+# for num, team in defTeams.items():
+#     if(teams_collection.find_one({'nickname': team})):
+#         thisTeam = teams_collection.find_one({'nickname': team})
+#         finalJson[thisTeam['name']] = {
+#             'team': thisTeam['_id'],
+#             'off_los_drive': driveJson['OFF.LOS/Dr'][num].split(' ')[0],
+#             'off_pts_redzone': offJson['Pts/RZ'][num].split(' ')[0],
+#             'def_pts_redzone': defJson['Pts/RZ'][num].split(' ')[0]
+#         }
+
+# # DEF pts/rz
+# for num, team in defTeams.items():
+#     if(teams_collection.find_one({'nickname': team})):
+#         thisTeam = teams_collection.find_one({'nickname': team})
+#         finalJson[thisTeam['name']] = {
+#             'team': thisTeam['_id'],
+#             'off_los_drive': driveJson['OFF.LOS/Dr'][num].split(' ')[0],
+#             'off_pts_redzone': offJson['Pts/RZ'][num].split(' ')[0],
+#             'def_pts_redzone': defJson['Pts/RZ'][num].split(' ')[0]
+#         }
+
+# 'off_los_drive': driveJson['OFF.LOS/Dr'][num].split(' ')[0],
+# 'off_pts_redzone': offJson['Pts/RZ'][num].split(' ')[0],
+
+print(finalJson)   
+
+# collection_fo.insert(finalJson)
+
+# Close DB
 client.close()
 
 
