b325457eec302890395c6dd8af8087d5c0e57aa1
Nick Connor <nickconnor52@gmail.com>
2018-07-20 09:59:54 -0400

    Can check each team in ESPN

--- python/espn/espnScrape.py
+++ python/espn/espnScrape.py
@@ -1,28 +1,4 @@
 import pandas as pd
-
-week = '0'
-
-# Give/Take Data
-tables = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/givetake", header=1)
-tables[0].to_json('../storage/espn/week-' + week + '/give_take.json')
-
-# OFF 3rd Down % Data
-tables = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/downs", header=1)
-tables[0].to_json('../storage/espn/week-' + week + '/OFF_3rd_pct.json')
-
-# DEF 3rd Down % Data
-tables = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/downs/position/defense", header=1)
-tables[0].to_json('../storage/espn/week-' + week + '/DEF_3rd_pct.json')
-
-# OFF Yds/game
-tables = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/total", header=0)
-tables[0].to_json('../storage/espn/week-' + week + '/OFF_yds_gm.json')
-
-# DEF Yds/game
-tables = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/total/position/defense", header=0)
-tables[0].to_json('../storage/espn/week-' + week + '/DEF_yds_gm.json')
-
-import pandas as pd
 import json
 from pymongo import MongoClient
 
@@ -30,17 +6,29 @@ from pymongo import MongoClient
 week = '0'
 season = '0'
 
-# LOS Data
+# LOS Data ---- FO
 tablesDrive = pd.read_html("http://www.footballoutsiders.com/stats/drivestats", header=0)
 driveJson = json.loads(tablesDrive[1].to_json())
 
-# OFF pts/RZ
-tablesOff = pd.read_html("http://www.footballoutsiders.com/stats/drivestatsoff", header=0)
-offJson = json.loads(tablesOff[1].to_json())
+# Give/Take Data
+tablesGive = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/givetake", header=1)
+giveData = json.loads(tablesGive[0].to_json())
 
-# DEF pts/RZ
-tablesDef = pd.read_html("http://www.footballoutsiders.com/stats/drivestatsdef2017", header=0)
-defJson = json.loads(tablesDef[1].to_json())
+# OFF 3rd Down % Data
+tablesOff3 = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/downs", header=1)
+off3Data = json.loads(tablesOff3[0].to_json())
+
+# DEF 3rd Down % Data
+tablesDef3 = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/downs/position/defense", header=1)
+def3Data = json.loads(tablesDef3[0].to_json())
+
+# OFF Yds/game
+tablesOffYds = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/total", header=0)
+offYdsData = json.loads(tablesOffYds[0].to_json())
+
+# DEF Yds/game
+tablesDefYds = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/total/position/defense", header=0)
+defYdsData = json.loads(tablesDefYds[0].to_json())
 
 # Connect to DB
 
@@ -52,38 +40,50 @@ teams_collection = db['teams']
 
 # Cleaned Up JSON
 
-defTeams = defJson['Team']
-driveTeams = driveJson['Team']
-offTeams = offJson['Team']
+def3Teams = def3Data['TEAM']
+off3Teams = off3Data['TEAM']
+giveTeams = giveData['TEAM']
+offYdsTeams = offYdsData['TEAM']
+defYdsTeams = defYdsData['TEAM']
 
 finalJson = {}
 
 # ADD Week Info
 thisWeek = weeks_collection.find_one({'number': week, 'season': season})
 
-# DRIVE pts/rz
-for num, team in driveTeams.items():
+# GIVE/TAKE
+for num, team in giveTeams.items():
     if(teams_collection.find_one({'nickname': team})):
+        print(team)
         thisTeam = teams_collection.find_one({'nickname': team})
         finalJson[thisTeam['name']] = {
             'team': thisTeam['_id'],
             'week': thisWeek['_id'],
-            'off_los_drive': driveJson['OFF.LOS/Dr'][num].split(' ')[0],
-            'def_los_drive': driveJson['DEF.LOS/Dr'][num].split(' ')[0],
+            'give_take_diff': giveData['DIFF'][num]
+        }
+    elif(teams_collection.find_one({'location': team})):
+        print(team)
+        thisTeam = teams_collection.find_one({'location': team})
+        finalJson[thisTeam['name']] = {
+            'team': thisTeam['_id'],
+            'week': thisWeek['_id'],
+            'give_take_diff': giveData['DIFF'][num]
         }
 
-# OFF pts/rz
-for num, team in offTeams.items():
-    if(teams_collection.find_one({'nickname': team})):
-        thisTeam = teams_collection.find_one({'nickname': team})
-        finalJson[thisTeam['name']]['off_pts_rz'] = offJson['Pts/RZ'][num].split(' ')[0]
+print()
 
-# DEF pts/rz
-for num, team in defTeams.items():
-    if(teams_collection.find_one({'nickname': team})):
-        thisTeam = teams_collection.find_one({'nickname': team})
-        finalJson[thisTeam['name']]['def_pts_rz'] = defJson['Pts/RZ'][num].split(' ')[0]
-        stats_collection.insert(finalJson[thisTeam['name']])
+# # OFF pts/rz
+# for num, team in offTeams.items():
+#     if(teams_collection.find_one({'nickname': team})):
+#         thisTeam = teams_collection.find_one({'nickname': team})
+#         finalJson[thisTeam['name']]['off_pts_rz'] = offJson['Pts/RZ'][num].split(' ')[0]
+
+# # DEF pts/rz
+# for num, team in defTeams.items():
+#     if(teams_collection.find_one({'nickname': team})):
+#         thisTeam = teams_collection.find_one({'nickname': team})
+#         finalJson[thisTeam['name']]['def_pts_rz'] = defJson['Pts/RZ'][num].split(' ')[0]
+#         stats_collection.insert(finalJson[thisTeam['name']])
 
 # Close DB
 client.close()
