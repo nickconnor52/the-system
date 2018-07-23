d4269565a2bb11edb0691d085e1de3417ea44f6c
Nick Connor <nickconnor52@gmail.com>
2018-07-23 16:15:28 -0400

    All data first pass should be complete

--- python/team-rankings/trScrape.py
+++ python/team-rankings/trScrape.py
@@ -2,9 +2,6 @@ import pandas as pd
 import json
 from pymongo import MongoClient
 
-
-import pandas as pd
-
 # TODO - add week dynamically for automation
 week = '0'
 season = '0'
@@ -13,13 +10,13 @@ season = '0'
 tablesGive = pd.read_html("http://www.espn.com/nfl/statistics/team/_/stat/givetake", header=1)
 giveData = json.loads(tablesGive[0].to_json())
 
-# OFF LOS Data
-tablesOffLOS = pd.read_html("https://www.teamrankings.com/nfl/stat/red-zone-scoring-attempts-per-game", header=0)
-offLOSData = json.loads(tablesOffLOS[0].to_json())
+# OFF RZA Data
+tablesOffRZA = pd.read_html("https://www.teamrankings.com/nfl/stat/red-zone-scoring-attempts-per-game", header=0)
+offRZAData = json.loads(tablesOffRZA[0].to_json())
 
-# DEF LOS Data
-tablesDefLOS = pd.read_html("https://www.teamrankings.com/nfl/stat/opponent-red-zone-scoring-attempts-per-game", header=0)
-defLOSData = json.loads(tablesDefLOS[0].to_json())
+# DEF RZA Data
+tablesDefRZA = pd.read_html("https://www.teamrankings.com/nfl/stat/opponent-red-zone-scoring-attempts-per-game", header=0)
+defRZAData = json.loads(tablesDefRZA[0].to_json())
 
 # Connect to DB
 
@@ -31,8 +28,8 @@ teams_collection = db['teams']
 
 # Cleaned Up JSON
 
-defTeams = defLOSData['Team']
-offTeams = offLOSData['Team']
+defTeams = defRZAData['Team']
+offTeams = offRZAData['Team']
 
 finalJson = {}
 
@@ -44,24 +41,24 @@ for num, team in offTeams.items():
     if(teams_collection.find_one({'nickname': team})):
         thisTeam = teams_collection.find_one({'nickname': team})
         finalJson[thisTeam['name']] = {
-            'off_RZA_game': str(giveData['DIFF'][num])
+            'off_RZA_game': str(offRZAData['2017'][num])
         }
     elif(teams_collection.find_one({'location': team})):
         thisTeam = teams_collection.find_one({'location': team})
         finalJson[thisTeam['name']] = {
-            'def_RZA_game': str(giveData['DIFF'][num])
+            'off_RZA_game': str(offRZAData['2017'][num])
         }
 
 # DEF Yds/Game
 for num, team in defTeams.items():
     if(teams_collection.find_one({'nickname': team})):
         thisTeam = teams_collection.find_one({'nickname': team})
-        finalJson[thisTeam['name']]['def_yds_game'] = str(defYdsData['YDS/G'][num])
+        finalJson[thisTeam['name']]['def_RZA_game'] = str(defRZAData['2017'][num])
         stats_collection.find_one_and_update({'team': thisTeam['_id']}, {'$set': finalJson[thisTeam['name']]})
 
     elif(teams_collection.find_one({'location': team})):
         thisTeam = teams_collection.find_one({'location': team})
-        finalJson[thisTeam['name']]['def_yds_game'] = str(defYdsData['YDS/G'][num])
+        finalJson[thisTeam['name']]['def_RZA_game'] = str(defRZAData['2017'][num])
         stats_collection.find_one_and_update({'team': thisTeam['_id']}, {'$set': finalJson[thisTeam['name']]})
 
 # Close DB
