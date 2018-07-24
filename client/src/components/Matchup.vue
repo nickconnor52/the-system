ff6939e2bb65558303a663e46e84deaec1567816
Nick Connor <nickconnor52@gmail.com>
2018-07-23 21:14:15 -0400

    Added matchup & spread styling

--- client/src/components/Matchup.vue
+++ client/src/components/Matchup.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="row">
-    <div class="offset-md-2 col-md-4">
-      <div class="card-body logo">
-      <img src="../assets/0020919_cincinnati-bengals_300.png" alt="bengals"/>
-      <h4 class="text-dark">{{matchup.homeTeam['name'] }}</h4>
-      </div>
-    </div>
+  <div class="row justify-content-md-center">
     <div class="col-md-4">
       <div class="card-body logo">
       <img src="../assets/0020920_dallas-cowboys_300.png" alt="bengals"/>
-      <h4 class="text-dark">{{ matchup.awayTeam['name'] }}</h4>
+      <h4 class="text-dark">{{matchup.awayTeam['location'] }} {{matchup.awayTeam['name'] }}</h4>
+      </div>
+    </div>
+    <div class="col-md-2 align-middle">
+      <h4 class="align-center">Spread: {{ matchup.vegasSpread }}</h4>
+    </div>
+    <div class="col-md-4">
+      <div class="card-body logo">
+      <img src="../assets/0020919_cincinnati-bengals_300.png" alt="bengals"/>
+      <h4 class="text-dark">{{matchup.homeTeam['location'] }} {{ matchup.homeTeam['name'] }}</h4>
       </div>
     </div>
   </div>
@@ -23,4 +26,14 @@ export default {
   ]
 }
 </script>
+
+<style>
+.align-center {
+  height: 100%;
+  display: flex; 
+  align-items: center;  /*Aligns vertically center */
+  justify-content: center; /*Aligns horizontally center */
+}
+</style>
+
 
