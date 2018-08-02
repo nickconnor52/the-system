86cc4fe82719f903ab473c4da0f58bd8fbb7c949
Nick Connor <nickconnor52@gmail.com>
2018-08-01 20:26:02 -0400

    Trying some shit

--- client/build/service-worker-prod.js
+++ client/build/service-worker-prod.js
@@ -12,7 +12,8 @@
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
     );
-
+  isLocalhost = false
+  
   window.addEventListener('load', function() {
       if ('serviceWorker' in navigator &&
           (window.location.protocol === 'https:' || isLocalhost)) {
