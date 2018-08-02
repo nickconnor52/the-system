47dd1965e20ee8f1ae56a81c20bc0665040c9305
Nick Connor <nickconnor52@gmail.com>
2018-08-01 21:48:22 -0400

    removed a private dir

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var globals = require('./private/globals');
+// var globals = require('./private/globals');
 require("dotenv").config()
 
 var indexRouter = require('./routes/index');
